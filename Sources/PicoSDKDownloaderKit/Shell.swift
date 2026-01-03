8ec1fe1337f45a65d925f6537066338517c72055
Gonzalo Larralde <132825+gonzalolarralde@users.noreply.github.com>
2026-01-03 12:24:51 -0300

    Remove @discardableResult from Shell.run
    The @discardableResult attribute was removed from the Shell.run method, indicating that the result of this function is now expected to be used or handled.

--- Sources/PicoSDKDownloaderKit/Shell.swift
+++ Sources/PicoSDKDownloaderKit/Shell.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 enum Shell {
-  @discardableResult
   static func run(_ executable: String, _ args: [String], cwd: URL? = nil) throws {
     let proc = Process()
     proc.executableURL = URL(fileURLWithPath: "/usr/bin/env")
